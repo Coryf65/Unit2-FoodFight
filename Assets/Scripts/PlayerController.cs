4ce99697e0f5c5475f0dc201306900374922262f
Cory Fabian <cory fabian coryf65@users.noreply.github.com>
2023-07-11 14:42:15 -0500

    created a boundary lines for the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
 
     [Range(1, 20)]
     [SerializeField] private float _speed = 1.0f;
+    [Range(1, 50)]
+    [Tooltip("Player boundary range of (+ x) to (- x) to what number is entered")]
+    [SerializeField] private float _boundaryRange = 10;
     public float _horizontalInput;
     
     // Start is called before the first frame update
@@ -18,6 +21,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -_boundaryRange)
+        {
+            transform.position = new Vector3(-10, transform.position.y);
+        }
+
+        if (transform.position.x > _boundaryRange)
+        {
+            transform.position = new Vector3(10, transform.position.y);
+        }
+        
         _horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * _horizontalInput * Time.deltaTime * _speed);
     }
