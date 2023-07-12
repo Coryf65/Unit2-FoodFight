10c4410e4e0d71eba1516d38ff952c8a76c07659
Cory Fabian <cory fabian coryf65@users.noreply.github.com>
2023-07-11 20:10:28 -0500

    out of bounds script setting boundaries for both the animals and the food

--- Assets/Scripts/OutOfBounds.cs
+++ Assets/Scripts/OutOfBounds.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class OutOfBounds : MonoBehaviour
 {
     [Header("Destroy this object when it goes out of bounds")] 
-    [SerializeField] private int _boundary = 50;
+    [SerializeField] private int _topBoundary = 50;
+    [SerializeField] private int _lowerBoundary = -10;
     
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > _boundary)
+        if (transform.position.z > _topBoundary || transform.position.z < _lowerBoundary)
         {
             Destroy(gameObject);
         }
